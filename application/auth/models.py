493f04784738143acd29fac19723687917702df1
larikkai <kai.larikka@kolumbus.fi>
2020-02-07 20:53:02 +0200

    Add leaderboard sql-query, fix main page url in layout.html

--- application/auth/models.py
+++ application/auth/models.py
@@ -28,7 +28,7 @@ class User(Base):
         return True
 
     @staticmethod
-    def find_users_with_no_tasks(done=True):
+    def find_users_with_no_games(done=False):
         stmt = text("SELECT Account.id, Account.name FROM Account"
                      " LEFT JOIN Game ON Game.account_id = Account.id"
                      " WHERE (Game.done IS null OR Game.done = :done)"
@@ -38,6 +38,22 @@ class User(Base):
 
         response = []
         for row in res:
-            response.append({"id":row[0], "name":row[1]})
+            response.append({"name":row[1]})
+
+        return response
+
+    @staticmethod
+    def list_users_by_games_played(done=True):
+        stmt = text("SELECT Account.id, Account.name, COUNT(*) FROM Account"
+                " LEFT JOIN Game ON Game.account_id = Account.id"
+                " WHERE (Game.done NOT null AND Game.done = :done)"
+                " GROUP BY Account.id"
+                " HAVING COUNT(Game.id) > 0"
+                " ORDER BY COUNT(*) DESC").params(done=done)
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"name":row[1], "games":row[2]})
 
         return response
