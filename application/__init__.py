b04a404a301ad51897abe195580a9fa542a331bf
larikkai <kai.larikka@kolumbus.fi>
2020-02-24 23:40:37 +0200

    Comment default users in application init.py

--- application/__init__.py
+++ application/__init__.py
@@ -89,7 +89,7 @@ try:
 
     if os.environ.get("HEROKU"):
         # Create default users
-        user1 = User('User user')
+        # user1 = User('User user')
         user1.username = 'test_user1'
         pw_hash = bcrypt.generate_password_hash('test123').decode('utf-8')
         user1.password = pw_hash
@@ -105,7 +105,7 @@ try:
         db.session().commit()
     
     else:
-        # Create default users
+        """ Create default users
         user1 = User('test user1')
         user1.username = 'test_user1'
         pw_hash = bcrypt.generate_password_hash('test').decode('utf-8')
@@ -134,6 +134,6 @@ try:
         user4.password = pw_hash
         user4.roles.append(user_role)
         db.session().add(user4)
-        db.session().commit()
+        db.session().commit() """
 except:
     pass
